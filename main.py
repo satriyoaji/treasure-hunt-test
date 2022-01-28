51b091440f09d9318233be1ded8120334effa2d1
satriyoaji27 <satriyoaji27@gmail.com>
2022-01-28 21:34:55 +0700

    call into main file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# MAIN DECLARATION HERE
+from app.game import Point
+from app.game import TreasureHunt
+
+load = Point(0, 0)
+game = TreasureHunt()
+game.initGame()
+game.tutorial()
+game.game_loop()
+print("Thanks for playing!")
